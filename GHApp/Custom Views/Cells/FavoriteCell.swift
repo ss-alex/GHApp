468594788663210461d0f559c6dce359c8ae96f3
Alexey Kirpichnikov <masonry1@yandex.ru>
2020-02-22 12:40:14 +0800

    First part:
    PersistenceManager file is created. This file connects default memory and Data that should be saved in there and retrieved from there.
    
    This data is the login and avatar image of the the GitHub user who was favorited.
    
    Function ‘retrieveFavorites’ add a new user to [Follower] array or throws an error of GFError type. This function decodes the data fetched from defaults.
    
    Function ‘save’ encodes it back to defaults.
    
    These two functions are the elements of one static function ‘updateWith’.
    
    2. When ‘updateWith’ is called, ‘PersistenceActionType’ is being checked (either it is .add or .remove).
    
    If ‘retrievedFavorites’ array does not contain a new favorited user & ‘.add’ actionType is swiped  a new user Data will be appended to the  ‘retrievedFavorites’ array.
    
    If ‘retrievedFavorites’ array contain a new favorited user & ‘.remove’ actionType is swiped, then the data about this user will be deleted from ‘retrievedFavorites’ array.
    
    Second part:
    
    In ‘FollowerListVC’ there is @objc func ‘addButtonTapped’. Inside that function function ‘getUserInfo’ is being called.
    
    If call is successful, ‘PersistenceManager.updateWith’ fucntion is called. ‘.add’ parameter is passed in there. If there is no error, a new user Data will be appended to the  ‘retrievedFavorites’ array and message about successful favoriting the  user will be sent to the screen.
    
    If call is unsuccessful, message about unsuccessful favoriting the user will be sent to the screen.
    
    Third part:
    
    ‘FavoriteCell’ is created programmatically. Inside there is a function ‘setTextAndImageFor’ that is created for downloading the image for the ‘avatarImageView’ and text for the ‘userNameLabel’ as 'login name.

---
+++ GHApp/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,56 @@
+//
+//  FavoriteCell.swift
+//  GHApp
+//
+//  Created by Лена Мырленко on 2020/2/22.
+//  Copyright © 2020 Alexey Kirpichnikov. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    static let reuseID = "FavoriteCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero) /// .zero because the constraints will be defined later
+    let userNameLabel = GFTittleLabel(textAlignment: .left, fontSize: 26)
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setTextAndImageFor(favorite: Follower) {
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+        userNameLabel.text = favorite.login
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(userNameLabel)
+        
+        accessoryType               = .disclosureIndicator /// small arrow on the right of the customed table view cell
+        let padding:CGFloat         = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            userNameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            userNameLabel.trailingAnchor.constraint(equalTo: self.leadingAnchor, constant: -padding),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 24),
+            userNameLabel.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+}
---
+++ GHApp/Managers/PersistenceManager.swift
@@ -0,0 +1,83 @@
+//
+//  PersistenceManager.swift
+//  GHApp
+//
+//  Created by Лена Мырленко on 2020/2/19.
+//  Copyright © 2020 Alexey Kirpichnikov. All rights reserved.
+//
+
+import Foundation
+
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favouritesKey = "favorites"
+    }
+    
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites{ result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType{
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                    
+                case .remove:
+                    retrievedFavorites.removeAll {$0.login == favorite.login} /// $0 = each item
+                }
+                
+                completed(save(favorites: retrievedFavorites))
+                
+                
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        /// as far as we save a complex array to the defaults, we will have to decode it to Data first, and when we use it - we decode it, at second /// [Follower] - [Favourites], a type of 'Follower''
+        
+        guard let favoritesData = defaults.object(forKey: Keys.favouritesKey) as? Data else {
+            completed(.success([])) /// it returns an empty array if the 'favoritesData' equals to 'nil'
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    
+    static func save(favorites: [Follower]) -> GFError? { /// if saving is successful, we gonna return 'nil'
+        
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favouritesKey)
+            return nil /// we are returning 'nil' because no error happens
+        } catch {
+            return .unableToFavorite /// if this encode fails, we can return '.unableToFavorite' type of error
+        }
+    }
+}
