711f6d404e00aa233971b026470dfdff1a75bf2d
Alexey Kirpichnikov <masonry1@yandex.ru>
2020-02-28 14:56:09 +0800

    The scrollView was added to the UserInfoVC to deal with problems in iPhone SE layout. Mainly, scrollview became the main view and content view were added there as a subView. ScrollView is scalable to fit the content within the small iPhone SE screen without changing the whole layout.

---
+++ GHApp/Extensions/UIView+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Ext.swift
+//  GHApp
+//
+//  Created by Лена Мырленко on 2020/2/28.
+//  Copyright © 2020 Alexey Kirpichnikov. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
+    
+}
+
